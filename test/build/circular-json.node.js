1f2c6edaa2d79c94397cf30abc1deed48a71f7a7
Andrea Giammarchi <agiammarchi@twitter.com>
2013-03-13 10:34:47 -0700

    automatic test generator

--- test/build/circular-json.node.js
+++ test/build/circular-json.node.js
@@ -1,3 +1,25 @@
+/*!
+Copyright (C) 2013 by WebReflection
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
 var
   // should be a not so common char
   // possibly one JSON does not encode
@@ -131,3 +153,5 @@ function stringifyRecursion(value, replacer, space) {
 function parseRecursion(text, reviver) {
   return JSON.parse(text, generateReviver(reviver));
 }
+this.stringify = stringifyRecursion;
+this.parse = parseRecursion;
